4fee5f97d7b316c5d45c514effeb630cc5577bfd
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-12-02 23:05:24 -0600

    input defined

---
+++ typescript/src/day1/q1.ts
@@ -0,0 +1,6 @@
+import { readFile } from "../utils";
+
+const input = "src/day1/input.txt"
+const data = readFile(input)
+
+console.log(data)
--- typescript/src/utils.ts
+++ typescript/src/utils.ts
@@ -1,11 +1,5 @@
 import * as fs from "fs"
 
-export const read_file = (file_path: string) => {
-  fs.readFile(file_path, "utf-8", (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    return data
-  })
+export const readFile = async (file_path: string) => {
+  return fs.readFileSync(file_path, "utf-8")
 }
