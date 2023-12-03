c29204d965fd768e6758346173ca1756b08d6ebb
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-12-02 23:31:44 -0600

    test process line

--- typescript/src/day1/q1.ts
+++ typescript/src/day1/q1.ts
@@ -1,6 +1,18 @@
+import { assert, warn } from "console";
 import { readFile } from "../utils";
 
-const input = "src/day1/input.txt"
-const data = readFile(input)
+// const input = "src/day1/input.txt"
+// const data = readFile(input)
 
-console.log(data)
+
+const processLine = (line): number => {
+  return 0
+}
+
+const test = () => {
+  assert (processLine("1abc2") == 12)
+  assert (processLine("pqr3stu8vwx") == 38)
+  assert (processLine("a1b2c3d4e5f") == 15)
+  assert (processLine("treb7uchet") == 77)
+}
+test()
