b2a82f2c378886b5bc0351a0fb96e438c412c0a1
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-12-02 22:40:53 -0600

    KISS

---
+++ typescript/README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2023 in TypeScript
+For now just trying to keep it simple :D
+## To run 
+`npm start <file_path>`
+e.g. `npm start src/day1/question1.ts`
--- typescript/src/day1/main.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs"
-
-fs.readFile("src/day1/test.txt", "utf-8", (err, data) => {
-  if (err){
-    console.error(err)
-    return
-  }
-  console.log(data)
-})
