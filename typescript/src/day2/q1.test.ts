3cbe02aaa7e7a81dfdafde64a10d68a8e3c893ba
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-12-04 21:40:28 -0600

    day2 question 1 done

--- typescript/src/day2/q1.test.ts
+++ typescript/src/day2/q1.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "@jest/globals"
-import { processLine } from "../day2/q1"
+import { limitReached, processLine, identifyColor, Colors } from "../day2/q1"
 
 const test_input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -7,13 +7,32 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
 
+describe("idenfyColor", () => {
+  let clrs: Colors = { r: 0, g: 0, b: 0 }
+  test("", () => {
+    identifyColor("1 blue", clrs)
+    identifyColor("23 red", clrs)
+    expect(clrs.b).toBe(1)
+    expect(clrs.r).toBe(23)
+  })
+})
+
+describe("limitReached", () => {
+  let clrs: Colors = { r: 2, g: 23, b: 0 }
+  let clrs2: Colors = { r: 2, g: 6, b: 3 }
+  test("", () => {
+    expect(limitReached(clrs)).toBe(true)
+    expect(limitReached(clrs2)).toBe(false)
+  })
+})
+
 describe("processLine", () => {
   const line_input: string[] = test_input.trim().split("\n")
-  const expected = [true, true, false, false, true]
+  const expected = [1, 2, 0, 0, 5]
   test("Given input", () => {
-    for(let i = 0; i < line_input.length; i++){
+    for (let i = 0; i < line_input.length; i++) {
+      console.log(line_input[i])
       expect(processLine(line_input[i])).toBe(expected[i])
     }
-
   })
 })
--- typescript/src/day2/q1.ts
+++ typescript/src/day2/q1.ts
@@ -1,20 +1,68 @@
 import { readFile } from "../utils";
 
-export const processLine = (line: string): boolean => {
-  return true
+export type Colors = {
+  r: number,
+  g: number,
+  b: number
+}
+
+const limits: Colors = {
+  r: 12,
+  g: 13,
+  b: 14
+}
+
+export const processLine = (line: string): number => {
+  const arr = line.trim().split(":")
+  const day_number = Number(arr[0].split(" ")[1])
+  const sprints = arr[1].trim().split(";")
+  for (let ea of sprints) {
+    let clrs: Colors = { r: 0, g: 0, b: 0 }
+    ea = ea.trim()
+    for (let i of ea.split(",")) {
+      identifyColor(i, clrs)
+      if (limitReached(clrs)){
+        return 0
+      }
+    }
+  }
+  return day_number
 }
 
 export const processFile = async (input: string): Promise<number> => {
   let total = 0
   for (let ea of input.trim().split('\n')) {
-    processLine(ea)
+    total += processLine(ea)
   }
   return total
 }
 
+export const limitReached = (c: Colors): boolean => {
+  for (let key of Object.keys(c))
+    if (c[key] > limits[key]) {
+      return true
+    }
+  return false
+}
+
+export const identifyColor = (s: string, clrs: Colors) => {
+  if (/red/.test(s)) {
+    clrs.r += Number(s.trim().split(" ")[0])
+
+  }
+  else if (/green/.test(s)) {
+    clrs.g += Number(s.trim().split(" ")[0])
+  }
+  else if (/blue/.test(s)) {
+    clrs.b += Number(s.trim().split(" ")[0])
+  }
+}
+
 const main = async (): Promise<number> => {
-  const input_file = "src/day1/input.txt"
+  const input_file = "src/day2/input.txt"
   const data = await readFile(input_file)
   let a = processFile(data)
   return a
 }
+
+export default main;
