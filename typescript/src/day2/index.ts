eaf24d33730628e9156423a656067450a3af05b2
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2023-12-04 21:55:49 -0600

    nice priting to presentation

--- typescript/src/day2/index.ts
+++ typescript/src/day2/index.ts
@@ -1,12 +1,4 @@
+import { printAnswers } from "../utils"
 import q1 from "./q1"
 
-const main = async () => {
-  const answer_q1 = await q1()
-  // const answer_q2 = await q2()
-  console.log("~~~~~ Day 2 ~~~~~")
-  console.log("Question 1:", answer_q1)
-  // console.log("Question 2:", answer_q2)
-  console.log("~~~~~~~~~~~~~~~~~~")
-}
-main()
-
+printAnswers(2, q1)
--- typescript/src/utils.ts
+++ typescript/src/utils.ts
@@ -3,3 +3,31 @@ import * as fs from "fs"
 export const readFile = async (file_path: string) => {
   return fs.readFileSync(file_path, "utf-8")
 }
+
+const printInColors = (color: string, s: any) => {
+  switch (color) {
+    case "red":
+      console.log(`\x1b[31m${s}\x1b[0m`)
+      break
+    case "blue":
+      console.log(`\x1b[34m${s}\x1b[0m`)
+      break
+    case "yellow":
+      console.log(`\x1b[33m${s}\x1b[0m`)
+  }
+}
+
+const blank = () => "To be done"
+
+
+export const printAnswers = async (day: string | number, q1: () => any = blank, q2: () => any = blank) => {
+  const answer_q1 = await q1()
+  const answer_q2 = await q2()
+  printInColors("blue", `~~~~~ Day ${day} ~~~~~`)
+  printInColors("red", "Question 1:")
+  printInColors("yellow", answer_q1)
+  printInColors("red", "Question 2:")
+  printInColors("yellow", answer_q2)
+ printInColors("blue", "~~~~~~~~~~~~~~~~~")
+}
+
