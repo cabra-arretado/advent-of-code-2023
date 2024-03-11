1e1670b86fa765cc45be67b36266bb2c3ff37103
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2024-03-11 11:29:41 -0500

    structure changes

--- python/day3.py
+++ python/day3.py
@@ -1,32 +1,34 @@
 #! /usr/bin/env python3
+# TO BE RUN IN THE ROOT FOLDER
 
-# Part 1
+## COMMON FOR BOTH PARTS
 
-with open('python/input.txt') as f:
+file = "inputs/day3_input.txt"
+with open(file) as f:
     lines = f.read().strip().split('\n')
     f_lines = list(lines)
 
-# Helper functions
 rows = len(f_lines)
 cols = len(f_lines[0])
 ARROUND_FILDS = [ (-1, -1), (-1, 0), (-1, 1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1) ]
 
+########### Part 1 ###########
 def is_symbol(c) -> bool:
 # return true if c is not a number or a '.'
-	return not c.isdigit() and c != '.'
+    return not c.isdigit() and c != '.'
 
 def touches(row, col, matrix):
 # return True if the position (row, col) touches a symbol
-	rows = len(matrix)
-	cols = len(matrix[0])
-	for i, j in ARROUND_FILDS:
-		if row + i < 0 or col + j < 0:
-			continue
-		if row + i >= rows or col + j >= cols:
-			continue
-		if is_symbol(matrix[row + i][col + j]):
-			return True
-	return False
+    rows = len(matrix)
+    cols = len(matrix[0])
+    for i, j in ARROUND_FILDS:
+        if row + i < 0 or col + j < 0:
+            continue
+        if row + i >= rows or col + j >= cols:
+            continue
+        if is_symbol(matrix[row + i][col + j]):
+            return True
+    return False
 
 touched = False
 acc = '0'
@@ -55,47 +57,48 @@ while row < rows:
 
 print(f"part 1: {total}")
 
+########### Part 2 ###########
 
 def find_number(row, col, matrix):
-	og_col = col
-	new_list = []
-	# format row-col
-	first_index = f'{row}-{col}'
+    og_col = col
+    new_list = []
+    # format row-col
+    first_index = f'{row}-{col}'
 
-	# <<<<
-	while col >= 0 and matrix[row][col].isdigit():
-		new_list.insert(0, matrix[row][col])
-		first_index = f'{row}-{col}'
-		col -= 1
-	# >>>>
-	col = og_col + 1
-	while col < cols and matrix[row][col].isdigit():
-		new_list.append(matrix[row][col])
-		col += 1
+    # <<<<
+    while col >= 0 and matrix[row][col].isdigit():
+        new_list.insert(0, matrix[row][col])
+        first_index = f'{row}-{col}'
+        col -= 1
+    # >>>>
+    col = og_col + 1
+    while col < cols and matrix[row][col].isdigit():
+        new_list.append(matrix[row][col])
+        col += 1
 
-	if not new_list:
-		return 1
-	return first_index, int(''.join(new_list))
+    if not new_list:
+        return 1
+    return first_index, int(''.join(new_list))
 
 
 def gear_ratio_processor(row, col, matrix):
-	# return the number of times the * touches the numbers
-	rows = len(matrix)
-	cols = len(matrix[0])
-	gears = dict()
-	for i, j in ARROUND_FILDS:
-		if row + i < 0 or col + j < 0:
-			continue
-		if row + i >= rows or col + j >= cols:
-			continue
-		if matrix[row + i][col + j].isdigit():
-			first_index, full_number = find_number(row + i, col + j, matrix)
-			gears.update({first_index: full_number})
+    # return the number of times the * touches the numbers
+    rows = len(matrix)
+    cols = len(matrix[0])
+    gears = dict()
+    for i, j in ARROUND_FILDS:
+        if row + i < 0 or col + j < 0:
+            continue
+        if row + i >= rows or col + j >= cols:
+            continue
+        if matrix[row + i][col + j].isdigit():
+            first_index, full_number = find_number(row + i, col + j, matrix)
+            gears.update({first_index: full_number})
 
-	if len(gears) < 2:
-		return 0
-	from functools import reduce
-	return reduce(lambda x, y: x * y, gears.values())
+    if len(gears) < 2:
+        return 0
+    from functools import reduce
+    return reduce(lambda x, y: x * y, gears.values())
 
 total = 0
 
