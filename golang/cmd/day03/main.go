fe03e8ccba2b9db5aead1f56a1264bcf1db5182e
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2024-06-22 12:13:20 -0500

    findFullNumber working with tests

--- golang/cmd/day03/main.go
+++ golang/cmd/day03/main.go
@@ -12,23 +12,25 @@ func part1(matrix [][]rune) int {
 }
 
 // findFullNumber finds the full number in the matrix
+// returns tuple: (row-col of the first digit, number)
 func findFullNumber(matrix [][]rune, row int, col int) (string, int) {
 	initIndex := col
-	number := ""
+	number := "" + string(matrix[row][col])
 	i := col + 1
-	for i < len(matrix) {
+	for i < len(matrix[row]) {
 		if unicode.IsDigit(matrix[row][i]) {
 			number += string(matrix[row][i])
-			col++
+			i++
 		} else {
 			break
 		}
 	}
-	for col > 0 {
-		if unicode.IsDigit(matrix[row][col]) {
-			number = string(matrix[row][col]) + number
-			initIndex = col
-			col--
+	i = initIndex - 1
+	for i >= 0 {
+		if unicode.IsDigit(matrix[row][i]) {
+			number = string(matrix[row][i]) + number
+			initIndex = i
+			i--
 		} else {
 			break
 		}
@@ -37,7 +39,7 @@ func findFullNumber(matrix [][]rune, row int, col int) (string, int) {
 	if err != nil {
 		panic(err)
 	}
-	return strconv.Itoa(initIndex) + "-" + strconv.Itoa(row), num
+	return strconv.Itoa(row)+ "-" + strconv.Itoa(initIndex) , num
 }
 
 func main() {
--- golang/cmd/day03/main_test.go
+++ golang/cmd/day03/main_test.go
@@ -8,13 +8,13 @@ func TestFindFullNumber(t *testing.T) {
 	matrix := [][]rune{
 		{'a', '1', '2', '3', 'b'},
 		{'c', '4', '5', '6', 'd'},
-		{'e', '7', '8', '9', 'f'},
+		{'5', 'k', 'i', 'u', '4'},
 	}
 
 	// Test case 1: Number in the middle of the row
-	expected1Index := "2-0"
+	expected1Index := "0-1"
 	expected1Num := 123
-	actual1Index, actual1Num := findFullNumber(matrix, 0, 1)
+	actual1Index, actual1Num := findFullNumber(matrix, 0, 3)
 	if actual1Index != expected1Index || actual1Num != expected1Num {
 		t.Errorf("Test case 1 failed. Expected (%s, %d), but got (%s, %d)", expected1Index, expected1Num, actual1Index, actual1Num)
 	}
@@ -27,11 +27,19 @@ func TestFindFullNumber(t *testing.T) {
 		t.Errorf("Test case 2 failed. Expected (%s, %d), but got (%s, %d)", expected2Index, expected2Num, actual2Index, actual2Num)
 	}
 
-	// Test case 3: Number at the end of the row
-	expected3Index := "3-2"
-	expected3Num := 789
-	actual3Index, actual3Num := findFullNumber(matrix, 2, 1)
+	// Test case 3: Number alone in the first column
+	expected3Index := "2-0"
+	expected3Num := 5
+	actual3Index, actual3Num := findFullNumber(matrix, 2, 0)
 	if actual3Index != expected3Index || actual3Num != expected3Num {
 		t.Errorf("Test case 3 failed. Expected (%s, %d), but got (%s, %d)", expected3Index, expected3Num, actual3Index, actual3Num)
+	}
+
+	// Test case 4: Number alone in the last column
+	expected4Index := "2-4"
+	expected4Num := 4
+	actual4Index, actual4Num := findFullNumber(matrix, 2, 4)
+	if actual3Index != expected3Index || actual3Num != expected3Num {
+		t.Errorf("Test case 4 failed. Expected (%s, %d), but got (%s, %d)", expected4Index, expected4Num, actual4Index, actual4Num)
 	}
 }
