869a90794817c9523b9acd90d828b9e161e884fa
Felipe <80273544+cabra-arretado@users.noreply.github.com>
2024-10-06 14:52:11 -0500

    comment some stuff

--- golang/cmd/day04/main.go
+++ golang/cmd/day04/main.go
@@ -1,20 +1,20 @@
 package day04
 
 import (
-	"advent-of-code-2023/utils"
+	// "advent-of-code-2023/utils"
 	"fmt"
 )
 
 func Day03() {
-	matrix := utils.ReadFileAsMatrix("3")
+	// matrix := utils.ReadFileAsMatrix("4")
+	fmt.Println("Part1")
 
+	result := "0"
 	// ---- Part 1 ----
-	result := Part1(matrix)
-	fmt.Println(result)
-	// fmt.Println("pass?", result == 544664)
+	// result := Part1(matrix)
+	fmt.Println("Part I:", result)
 
 	// ---- Part 2 ----
-	result = Part2(matrix)
-	fmt.Println(result)
-	// fmt.Println("pass?", result == 84495585)
+	// result = Part2(matrix)
+	fmt.Println("Part II:", result)
 }
