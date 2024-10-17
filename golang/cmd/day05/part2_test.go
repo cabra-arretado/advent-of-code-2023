bbc5459d485b2618164be6383e9eec3c1c176f57
Felipe M <80273544+cabra-arretado@users.noreply.github.com>
2024-10-17 09:55:40 -0500

    before

--- golang/cmd/day05/part2_test.go
+++ golang/cmd/day05/part2_test.go
@@ -2,10 +2,10 @@ package day05
 
 import (
 	"fmt"
-	"testing"
 	"reflect"
-	"strings"
 	"strconv"
+	"strings"
+	"testing"
 )
 
 func compareSlices(slice1 []int, slice2 []int) bool {
@@ -53,31 +53,19 @@ func TestDay05PartII(t *testing.T) {
 
 	//Let's get all the seeds
 	seeds := make([]int, 0)
-	seedLines := strings.Trim(seedsChunk, "seeds: ")
-	duo := make([]int, 0)
-	for _, line := range strings.Split(seedLines, "\n") {
-		for _, seed := range strings.Split(line, " ") {
-			seedNumber, err := strconv.Atoi(seed)
-			if err != nil {
-				panic(fmt.Sprintf("Error converting seed to int: %v\n", seed))
-			}
-			if len(duo) < 2 {
-				duo = append(duo, seedNumber)
-			} else {
-				start := duo[0]
-				length := duo[1] - duo[0] + 1
-				for i := start; i <= length; i++ {
-					seeds = append(seeds, i)
-				}
-				duo = make([]int, 0)
-			}
-		}
-	}
+	seedLines := strings.TrimPrefix(seedsChunk, "seeds: ")
+	fmt.Println("seedLines", seedLines)
+	// for _, line := range seedLines {
+	// 	if (len(seedNumbers) % 2) != 0 {
+	// 		panic(fmt.Sprintf("Error: seeds are not in pairs: %v\n", seedNumbers))
+	// 	}
+	// }
+
 	actualSeeds := seeds
-	expected := []int{79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67}
+	expectedSeeds := []int{79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67}
 	fmt.Println("actual", actualSeeds)
-	fmt.Println("expected", expected)
-	compare := compareSlices(actualSeeds, expected)
+	fmt.Println("expected", expectedSeeds)
+	compare := compareSlices(actualSeeds, expectedSeeds)
 	fmt.Println("compare", compare)
 
 }
