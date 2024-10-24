e8d9c76462e07fd7da42e6334dcb32d8b95952e1
Felipe M <80273544+cabra-arretado@users.noreply.github.com>
2024-10-24 14:41:29 -0500

    adds better handling for the seed processing

--- golang/cmd/day05/part2.go
+++ golang/cmd/day05/part2.go
@@ -7,8 +7,8 @@ import (
 )
 
 type SeedRange struct {
-	seedStart int
-	length    int
+	start int
+	end   int
 }
 
 type Instructions struct {
@@ -31,7 +31,7 @@ func getSeeds(seedsChunk string) []SeedRange {
 			}
 			duo = append(duo, seedInt)
 			if len(duo) == 2 {
-				seedRange := SeedRange{seedStart: duo[0], length: duo[1]}
+				seedRange := SeedRange{start: duo[0], end: duo[0] + duo[1]}
 				duo = make([]int, 0, 2)
 				seeds = append(seeds, seedRange)
 			}
@@ -40,7 +40,26 @@ func getSeeds(seedsChunk string) []SeedRange {
 	return seeds
 }
 
+func getInstructions(chunk string) []Instructions {
+	chunckLines := strings.Split(chunk, "\n")
+	chunckLines = chunckLines[1:]
+	instructions := make([]Instructions, 0)
+	for _, line := range chunckLines {
+		lineTrimed := strings.TrimSpace(line)
+		var destinationStart, sourceStart, rangeLength int
+		_, err := fmt.Sscanf("%d %d %d", lineTrimed, &destinationStart, &sourceStart, &rangeLength)
+		if err != nil {
+			fmt.Printf("error reading instructions %v", err)
+		}
+		instructions = append(instructions, Instructions{destinationStart: destinationStart, sourceStart: sourceStart, rangeLength: rangeLength})
+	}
+	return instructions
+}
+
 func Part2(file string) int {
+	chunks := strings.Split(file, "\n\n")
+	// seeds := getSeeds(chunks[0])
+	chunks = chunks[1:]
 
 	return 0
 }
--- golang/cmd/day05/part2_test.go
+++ golang/cmd/day05/part2_test.go
@@ -48,7 +48,7 @@ func compareSlices(slice1 []int, slice2 []int) bool {
 
 func TestGetSeeds(t *testing.T) {
 	seeds := getSeeds("seeds: 79 14 55 13")
-	expected := []SeedRange{{79, 14}, {55, 13}}
+	expected := []SeedRange{{79, 93}, {55, 68}}
 	fmt.Println("Seeds: ", seeds)
 	fmt.Println("Expected: ", expected)
 	fmt.Println("Passes?", reflect.DeepEqual(seeds, expected))
@@ -56,10 +56,18 @@ func TestGetSeeds(t *testing.T) {
 18 9 199 231`
 
 	seeds2 := getSeeds(secondExample)
-	expected2 := []SeedRange{{79, 14}, {55, 13}, {18, 9}, {199, 231}}
+	expected2 := []SeedRange{{79, 93}, {55, 68}, {18, 27}, {199, 430}}
 	fmt.Println("Seeds2: ", seeds2)
 	fmt.Println("Expected2: ", expected2)
 	fmt.Println("Passes?", reflect.DeepEqual(seeds2, expected2))
+}
+
+func TestGetInstructions(t *testing.T) {
+	instructions := getInstructions("50 98 2\n52 50 48")
+	expected := []Instructions{{50, 98, 2}, {52, 50, 48}}
+	fmt.Println("Instructions: ", instructions)
+	fmt.Println("Expected: ", expected)
+	fmt.Println("Passes?", reflect.DeepEqual(instructions, expected))
 }
 
 func TestPartII(t *testing.T) {
